0d4badd433b5c0099a9207ce56baa5dee7e27397
Seth Geyer <seth.geyer@gmail.com>
2014-08-25 19:46:16 -0600

    completed stories on adding todos and deleting them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,66 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $('body').append("<h1>hello world, I'm from only javascript!</h1>");
+
+  var $body = $('body')
+    $body.prepend("<h1 class='center' >Todoly</h1>");
+
+
+  var sectionBreak = "<hr/>";
+  var listSection =   "<h3 id='todos' class='col'>Todo!</h3>";
+  var todoList = "<ul id='todo-list'></ul>";
+  var toDoListSection = "<div id='header'>" + sectionBreak + listSection + "</div>";
+
+  var xButton = "<div class='remove-button'>x</div>";
+  var flash = "<div class='flash-text'>Todo Created</div>";
+  var flashContainer = "<div class='col flash container'>" + flash + xButton + "</div>";
+
+  var flashMessage = "Todo Created";
+  var xButton = "<div class='remove-button'>x</div>";
+  var flash = "<div class='flash-text'>" + flashMessage + "</div>";
+  var flashContainer = "<div class='col flash container'>" + flash + xButton + "</div>";
+
+
+
+
+  $body.append(toDoListSection);
+  var $header = $('#header');
+   $header.append(flashContainer);
+   $header.append(todoList);
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+
+    var removeButton = "<div class='red remove-button'>x</div>";
+    var todoItem = "<div class='item'>" + $('#todo-item').val() + "</div>";
+    var itemContainer = "<div class='container'>" + todoItem + removeButton + "</div>";
+
+    $('ul').append(itemContainer);
+
+    if ($header.css('display') == 'none') {
+      $header.toggle()
+    }
+
+    $flash = $('.flash')
+    $flash.toggle();
+    $flash.fadeOut(1000)
+
+  });
+
+
+  $body.on('click','.remove-button', function() {
+    $(this).parents('.container').toggle()
+
+    if ($(this).siblings('.item')) {
+//      $flash.toggle()
+//      $flash.fadeOut(1000)
+//the above currently flashes green
+    }
+
+  })
+
+
+
+
 });
